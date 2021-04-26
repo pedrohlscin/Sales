5940b8b0432bb72252e91ddc33b58421681079a6
Pedro Henrique <phls@cin.ufpe.br>
2021-04-26 15:13:16 -0300

    fixing last time things

--- src/main/java/com/pedroso/sales/controller/SalesController.java
+++ src/main/java/com/pedroso/sales/controller/SalesController.java
@@ -2,21 +2,23 @@ package com.pedroso.sales.controller;
 
 import com.pedroso.sales.model.Product;
 import com.pedroso.sales.model.ProductsTax;
-import com.pedroso.sales.service.ProductService;
+import com.pedroso.sales.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.List;
 
 @RestController
 public class SalesController {
 
     @Autowired
-    private ProductService service;
+    ProductService service;
 
     @PostMapping("/product")
     @ResponseBody
--- src/main/java/com/pedroso/sales/service/ProductService.java
+++ src/main/java/com/pedroso/sales/service/ProductService.java
@@ -5,24 +5,25 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.List;
 
 @Service
 public class ProductService {
 
     public List<Product> calculateTaxes(List<Product> products){
-        BigDecimal onePointOne = new BigDecimal(1.1);
-        BigDecimal onePointOFive = new BigDecimal(1.05);
+        BigDecimal pointOFive = new BigDecimal("0.05");
         MathContext m = new MathContext(2);
 
         for(Product p : products){
+            int tax = 0;
             if(p.isHasBasicTax()){
-                p.setTax(new BigDecimal(p.getValue().multiply(onePointOne).subtract(p.getValue()).round(m)+ ""));
+                tax += 10;
             }
-            if(p.isImported()){
-                p.setTax(new BigDecimal(p.getTax().add(p.getValue().multiply(onePointOFive).subtract(p.getValue()).round(m))+ ""));
+            if(p.isImported()) {
+                tax += 5;
             }
+            BigDecimal taxes = roundPointOFive(p.getValue().multiply(new BigDecimal(((float)tax/(float)100)+"")), pointOFive, m);
+            p.setTax(taxes);
         }
         return products;
     }
